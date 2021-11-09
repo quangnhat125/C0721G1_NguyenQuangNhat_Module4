b5d1b57f6a0f2a204fce6b4974fdc09742163a7a
quangnhatnguyen1405@gmail.com <quangnhatnguyen1405@gmail.com>
2021-11-09 23:00:52 +0700

    update

---
+++ bai_9/thuc_hanh/thuc_hanh_1/bo_sung_log_cho_ung_dung_quan_li/src/main/java/codegym/formatter/ProvinceFormatter.java
@@ -0,0 +1,34 @@
+package codegym.formatter;
+
+import codegym.model.Province;
+import codegym.service.ProvinceService;
+import org.hibernate.engine.jdbc.internal.Formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+
+
+public class ProvinceFormatter implements Formatter<Province> {
+    private final ProvinceService provinceService;
+
+    public ProvinceFormatter(ProvinceService provinceService) {
+        this.provinceService = provinceService;
+    }
+
+    @Override
+    public Province parse(String text, Locale locale) throws ParseException {
+        Optional<Province> provinceOptional = provinceService.findOne(Long.valueOf(text));
+        return provinceOptional.orElse(null);
+    }
+
+    @Override
+    public String print(Province object, Locale locale) {
+        return object.toString();
+    }
+
+    @Override
+    public String format(String source) {
+        return null;
+    }
+}
---
+++ bai_9/thuc_hanh/thuc_hanh_1/bo_sung_log_cho_ung_dung_quan_li/src/main/java/codegym/repository/ProvinceRepository.java
@@ -0,0 +1,11 @@
+package codegym.repository;
+
+
+
+import codegym.model.Province;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends CrudRepository<Province, Long> {
+}
